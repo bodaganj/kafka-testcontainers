5251c5de04092a6ff4eff4020097c9690dd7c0a1
ganzhab <bodaganj@gmail.com>
2021-03-19 17:09:42 +0100

    initial commit

---
+++ src/test/java/com/bodaganj/KafkaTestContainersTest.java
@@ -0,0 +1,15 @@
+package com.bodaganj;
+
+import org.junit.jupiter.api.Test;
+
+public class KafkaTestContainersTest {
+
+   @Test
+   void checkMessageIsProducedAndConsumedToDockerizedKafka() {
+      // Given: generate avro message
+
+      // When: message is sent to dockerized kafka
+
+      // Then: message is consumed from dockerized kafka
+   }
+}
